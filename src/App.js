96618731319acc3e9ded594df77e3d0675557f27
Mateus Levy <levymateus06@Gmail.com>
2023-06-19 01:21:09 -0300

    adicionando pastas

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import gameData from "./utils/data/data";
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      Game-Insight
+    <button onClick={()=>{
+   
+    }}>
+
+
+    </button>
+
+    
+    </div>
+  );
+}
+
+export default App;
